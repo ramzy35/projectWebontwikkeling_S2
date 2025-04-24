8b49865f6238e3c934875a9ddd762d204c1f232c
Lamlyj Ramzy (s156986) <ramzy.lamlyj@student.ap.be>
2025-04-24 13:28:47 +0000

    ready?

--- TerminalApp/index.ts
+++ TerminalApp/index.ts
@@ -4,58 +4,80 @@ import * as readline from 'readline-sync';
 import { parse } from "path";
 
 
-function displayMenu(): void {
-    console.log ("\nwelcome to jsjon data viewer");
-    console.log("1. view all data");
-    console.log("2. filter by id ");
-    console.log("3. exit");
-    promtUser();
+// function displayMenu(): void {
+//     console.log ("\nwelcome to jsjon data viewer");
+//     console.log("1. view all data");
+//     console.log("2. filter by id ");
+//     console.log("3. exit");
+//     promtUser();
+// }
+
+function pressSpace(){
+    console.log("\nPress enter to continue.")
+    readline.question("");
 }
 
 function promtUser(): void {
-    const res = readline.question("\nplease enter your choice");
-    switch (res) {
-        case "1":
-            cardData.forEach(card => {
-                console.log(`\n- ${card.name} (${card.id})`);
-            });
-
-            displayMenu();
-            break;
-        case "2":
-            const choiceId = readline.question("\nPlease enter the id you want to filter by: ");
-            const found: Card[] = cardData.filter(card => {
-                return card.id === choiceId
-            });
-            if (found.length === 0 ) {
-                console.log("No cards found with that ID")
-            }else {
-            console.log(`\n- ${found[0].name} (${found[0].id})`);
-            console.log(`- Description: ${found[0].description}`);
-            console.log(`- powerLevel: ${found[0].powerLevel}`);
-            console.log(`- isLegendary: ${found[0].isLegendary}`);
-            console.log(`- rarity: ${found[0].rarity}`);
-            console.log(`- manaCost: ${found[0].manaCost}`);
-            console.log(`- cardGame: ${found[0].cardGame}`);
-            console.log(`- cardSet: ${found[0].cardSet}`);
-            console.log(`- abilities: ${found[0].abilities.join(', ')}`);
-            console.log(`- faction: ${found[0].faction.name}`);
-            console.log(`- alignment: ${found[0].faction.alignment}`);
-            console.log(`- foundedYear: ${found[0].faction.foundedYear}`);
-            console.log(`- isSecretSociety: ${found[0].faction.isSecretSociety}`);
-            }
-            break;
-
-        case "3":
-
-            break;
     
-        default:
-            break;
+    let keepGoing: boolean = true;
+
+    while (keepGoing) {
+
+        console.log ("\nwelcome to jsjon data viewer");
+        console.log("1. view all data");
+        console.log("2. filter by id ");
+        console.log("3. exit");
+        
+        const res = readline.question("\nplease enter your choice: ");
+        switch (res) {
+            case "1":
+
+                cardData.forEach(card => {
+                    console.log(`\n- ${card.name} (${card.id})`);
+                });
+                // displayMenu();
+                pressSpace();
+                break;
+
+            case "2":
+
+                const choiceId = readline.question("\nPlease enter the id you want to filter by: ");
+                const found: Card[] = cardData.filter(card => {
+
+                    return card.id === choiceId
+
+                });
+
+                if (found.length === 0 ) {
+                    console.log(`\nNo cards found with that ID: ${choiceId}`)
+                }
+                else {
+                    console.log(`\n- ${found[0].name} (${found[0].id})`);
+                    console.log(`- Description: ${found[0].description}`);
+                    console.log(`- powerLevel: ${found[0].powerLevel}`);
+                    console.log(`- isLegendary: ${found[0].isLegendary}`);
+                    console.log(`- rarity: ${found[0].rarity}`);
+                    console.log(`- manaCost: ${found[0].manaCost}`);
+                    console.log(`- cardGame: ${found[0].cardGame}`);
+                    console.log(`- cardSet: ${found[0].cardSet}`);
+                    console.log(`- abilities: ${found[0].abilities.join(', ')}`);
+                    console.log(`- faction: ${found[0].faction.name}`);
+                    console.log(`- alignment: ${found[0].faction.alignment}`);
+                    console.log(`- foundedYear: ${found[0].faction.foundedYear}`);
+                    console.log(`- isSecretSociety: ${found[0].faction.isSecretSociety}`);
+                }
+                pressSpace();
+                break;
+            case "3":
+                keepGoing = false;
+                break;
+            default:
+                break;
+        }
     }
 }
 
-displayMenu();
+promtUser();
 
 
 
